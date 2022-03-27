45f88a68bef618439d5ef85e3e59a120ebff686c
corben78 <corben.dallas@gmx.at>
2022-03-27 22:09:23 +0200

    Install websocket server and establish connection
    *index.js:
        Implement websocket server, to work with SRM
    
    *package-lock.json,
    *package.json:
        'npm install ws'

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const tmi = require('tmi.js');
+const WebSocket = require('ws');
 
 // options set by user
 let options = JSON.parse(fs.readFileSync('./config.json'));
@@ -8,12 +9,25 @@ twitch = new tmi.client(options.twitch);
 twitch.connect();
 
 twitch.on('chat', function(c, u, m, s) {
-    // Don't listen to my own messages..
-    if (s) return;
+	// check for allowed commands
 
     onChatHandler(c, u, m);
 });
 
+
+const wss = new WebSocket.Server({
+    port: 9090,
+    path: '/SRM'
+});
+
+wss.on('connection', function connection(ws) {
+
+    ws.on('message', function incoming(message) {
+    	twitch.say(options.twitch.channels[0], String(message));
+    });
+
+});
+
 function onChatHandler(channel, userstate, message) {
-	//
+
 }
